fc7aeff690f35bc3ad78aed669c7964bff971d8c
Grace Anconetani <grace.anconetani@gmail.com>
2019-10-22 14:09:26 -0500

    my edit

---
+++ client/build/precache-manifest.018dafb541873a07fe386c9e9701129c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84cbfaef6f1ff16c599d72bc5d122060",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38881bf7f70ba16deef8",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "27ac796a5aaa3487c120",
+    "url": "/static/js/2.032f6863.chunk.js"
+  },
+  {
+    "revision": "38881bf7f70ba16deef8",
+    "url": "/static/js/main.201fe200.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
