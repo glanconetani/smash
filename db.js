e609b39716cb178bb6a42d28a4e8e44f4442d66c
Maximiliano Perez <maxp98@utexas.edu>
2019-10-23 18:14:03 -0500

    saved all character thumbnails

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 var mysql = require('mysql')
 var buffer = require('buffer')
+var atob = require('atob')
+var btoa = require('btoa')
+var fs = require('fs')
 var request = require('request').defaults({ encoding: null})
 const fetch = require("node-fetch")
+
 
 var con = mysql.createConnection({
     host: 'smash-project.cwrpa3uglhdr.us-east-2.rds.amazonaws.com',
@@ -10,16 +14,27 @@ var con = mysql.createConnection({
     database: 'smash'
 });
 
-// var options = {
-//     method: 'GET',
-//     headers: {'Content-Type' :  'application/x-www-form-urlencoded',
-//     },
-//     mode: 'cors',
-//     cache: 'default'
-// }
+
+
 
 con.connect()
 con.query('SELECT * FROM smash.characterInfo', function (err, rows, fields){
+    for (i = 0; i < 58; i++){
+        request.get(rows[0].main_image_url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+            }
+        }).pipe(fs.createWriteStream('./images/full/' + (rows[i].name) + '.png'))
+    }
+
+    for (i = 0; i < 58; i++){
+        request.get(rows[0].thumbnail_url, function(error, response, body) {
+            if (!error && response.statusCode == 200) {
+                data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
+            }
+        }).pipe(fs.createWriteStream('./images/thumbnails/' + (rows[i].name) + '.png'))
+    }
+    
     let characterData = {
         //string
         name:"",
@@ -35,64 +50,23 @@ con.query('SELECT * FROM smash.characterInfo', function (err, rows, fields){
 
         //hex
         color_theme: "",
-
-        // movements, // JSON Array?
-        // attributes, // JSON Array?
-        // unique_properties, //JSON Array?
     }
     
     if (err) throw err
 
-    //show image url
-    //console.log(rows[0].main_image_url)
 
     characterData.name = rows[0].name
-    characterData.displayName = rows[0].displayName
-    
-    var request = require('request').defaults({ encoding: null})
+    characterData.displayName = rows[0].display_name
 
-    request.get(rows[0].main_image_url, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            data = "data:" + response.headers["content-type"] + ";base64," + new Buffer(body).toString('base64');
-            console.log(data);
-            //characterData.main_image = JSON.stringify(data)
-            
-        }
-    })
-
-    request.get(rows[0].thumbnail_image, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            //data = "data:" + response.headers["content-type"] + ";base64," + new Buffer.alloc(buffer.constants.MAX_LENGTH, body).toString('base64');
-            //console.log(data);
-            //characterData.thumbnail_image = JSON.stringify(data)
-        }
-    })
     
+    characterData.thumbnail_image = "hello"
+
     characterData.color_theme = rows[0].color_theme
 
     let jsonString = JSON.stringify(characterData)
-    //console.log(jsonString)
-    
-
-    //
-    // fetch(rows[0].main_image_url, options).then((response) => {
-    //     response.arrayBuffer().then((buffer) => {
-    //         console.log(1)
-    //         var base64Flag = 'data:image/png;base64'
-    //         var imageStr = arrayBufferToBase64(buffer)
-    //         console.log(imageStr)
-    //         //document.querySelector('img').src = base64Flag + imageStr;
-    //     });
-    // });
-
-    // function arrayBufferToBase64(buffer) {
-    //     var binary = ''
-    //     var bytes = [].slice.call(new Uint8Array(buffer))
-
-    //     bytes.forEach((b) => binary  += String.fromCharCode(b));
-        
-    //     return window.btoa(binary);
-    // }     
+   // console.log(characterData.thumbnail_image)
+    console.log(jsonString)
+    //atob()
 })
 
 
