d485e7ba74f466e8c145a6eadb505be71f09545b
dannymaclean98 <dannymaclean98@gmail.com>
2019-11-11 17:04:28 -0600

    first push to branch with Search stuff

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,15 +3,14 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom'
 import SmashTable from '../Components/SmashTable.jsx'
 import NavBar from '../Components/NavigationBar.jsx'
-
-
+import Search from '../Components/Search.jsx'
 
 export default class HomePage extends Component {
   render() {
     return (
       <div>
         <NavBar />
-        
+        <Search />
         <SmashTable />
       </div>
     );
