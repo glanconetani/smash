059235b4938fc50043dca1b301717ac4bc9b93a7
Esha Makwana <esha.makwana@utexas.edu>
2019-11-15 16:00:58 -0600

    routes try 2

---
+++ server/routes.js
@@ -0,0 +1,230 @@
+export default [
+    {
+        name: "Bowser",
+        path: "/Bowser",
+    },
+    {
+        name: "BowserJr",
+        path: "/BowserJr",
+    },
+    {
+        name: "CaptainFalcon",
+        path: "/CaptainFalcon",
+    },
+    {
+        name: "Chizard",
+        path: "/Chizard",
+    },
+    {
+        name: "Cloud",
+        path: "/Cloud",
+    },
+    {
+        name: "Corrin",
+        path: "/Corrin",
+    },
+    {
+        name: "DarkPit",
+        path: "/DarkPit",
+    },
+    {
+        name: "DiddyKong",
+        path: "/DiddyKong",
+    },
+    {
+        name: "DonkeyKong",
+        path: "/DonkeyKong",
+    },
+    {
+        name: "DrMario",
+        path: "/DrMario",
+    },
+    {
+        name: "DuckHunt",
+        path: "/DuckHunt",
+    },
+    {
+        name: "Falco",
+        path: "/Falco",
+    },
+    {
+        name: "Fox",
+        path: "/Fox",
+    },
+    {
+        name: "GameWatch",
+        path: "/GameWatch", //took out & 
+    },
+    {
+        name: "Ganondorf",
+        path: "/Ganondorf",
+    },
+    {
+        name: "Greninja",
+        path: "/Greninja",
+    },
+    {
+        name: "Ike",
+        path: "/Ike",
+    },
+    {
+        name: "Jigglypuff",
+        path: "/Jigglypuff",
+    },
+    {
+        name: "KingDedede",
+        path: "/KingDedede",
+    },
+    {
+        name: "Kirby",
+        path: "/Kirby",
+    },
+    {
+        name: "Link",
+        path: "/Link",
+    },
+    {
+        name: "LittleMac",
+        path: "/LittleMac",
+    },
+    {
+        name: "Lucario",
+        path: "/Lucario",
+    },
+    {
+        name: "Lucas",
+        path: "/Lucas",
+    },
+    {
+        name: "Lucina",
+        path: "/Lucina",
+    },
+    {
+        name: "Luigi",
+        path: "/Luigi",
+    },
+    {
+        name: "Mario",
+        path: "/Mario",
+    },
+    {
+        name: "Marth",
+        path: "/Marth",
+    },
+    {
+        name: "MegaMan",
+        path: "/MegaMan",
+    },
+    {
+        name: "MetaKnight",
+        path: "/MetaKnight",
+    },
+    {
+        name: "Mewtwo",
+        path: "/Mewtwo",
+    },
+    {
+        name: "MiiBrawler",
+        path: "/MiiBrawler",
+    },
+    {
+        name: "MiiGunner",
+        path: "/MiiGunner",
+    },
+    {
+        name: "MiiSwordfighter",
+        path: "/MiiSwordfighter",
+    },
+    {
+        name: "Ness",
+        path: "/Ness",
+    },
+    {
+        name: "Olimar",
+        path: "/Olimar",
+    },
+    {
+        name: "PACMAN",
+        path: "/PACMAN", //took out -
+    },
+    {
+        name: "Palutena",
+        path: "/Palutena",
+    },
+    {
+        name: "Peach",
+        path: "/Peach",
+    },
+    {
+        name: "Pikachu",
+        path: "/Pikachu",
+    },
+    {
+        name: "Pit",
+        path: "/Pit",
+    },
+    {
+        name: "ROB",
+        path: "/ROB",//took out . 
+    },
+    {
+        name: "Robin",
+        path: "/Robin",
+    },
+    {
+        name: "RosalinaLuma",
+        path: "/RosalinaLuma", //took out &
+    },
+    {
+        name: "Roy",
+        path: "/Roy",
+    },
+    {
+        name: "Ryu",
+        path: "/Ryu",
+    },
+    {
+        name: "Samus",
+        path: "/Samus",
+    },
+    {
+        name: "Sheik",
+        path: "/Sheik",
+    },
+    {
+        name: "Shulk",
+        path: "/Shulk",
+    },
+    {
+        name: "Sonic",
+        path: "/Sonic",
+    },
+    {
+        name: "ToonLink",
+        path: "/ToonLink",
+    },
+    {
+        name: "Villager",
+        path: "/Villager",
+    },
+    {
+        name: "Wario",
+        path: "/Wario",
+    },
+    {
+        name: "WiiFitTrainer",
+        path: "/WiiFitTrainer",
+    },
+    {
+        name: "Yoshi",
+        path: "/Yoshi",
+    },
+    {
+        name: "Zelda",
+        path: "/Zelda",
+    },
+    {
+        name: "ZeroSuitSamus",
+        path: "/ZeroSuitSamus",
+    },
+];    
