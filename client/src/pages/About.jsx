7edf0101ae6bc5d6594916a0aeb18a3d42ff54f5
Maximiliano Perez <maxp98@utexas.edu>
2019-11-15 22:10:19 -0600

    git commits updated

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -22,7 +22,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-9/28059176_1556472821133744_5384015223063068067_n.jpg?_nc_cat=100&_nc_oc=AQnx6lhAvB6JuelkLNIZ5dO4h7gFfbWUEyK9CvSc1ZNud7G8pRM5teLalo1t5IpjV_0&_nc_ht=scontent.fftw1-1.fna&oh=69e4b8bfaea02e82e4fe009c82648cab&oe=5E53BFAA" />
             <p>Grace is a 4th year ECE major with tech cores in Software and Academic Enrichment. Grace
             is responsible for front-end design. </p>
-            <p>Grace has a total of 9 commits and 0 unit tests.</p>
+            <p>Grace has a total of 17 commits and 0 unit tests.</p>
           </div>
 
           <br></br>
@@ -33,7 +33,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-9/16711644_10202879577186791_1824655584936966991_n.jpg?_nc_cat=109&_nc_oc=AQkA3x1kapQGXu2F_Dg-MT3EbqMvnUehJ1OQEsKXhqTMC3rb87GNnowCwY_fCzq3d18&_nc_ht=scontent.fftw1-1.fna&oh=cbcaf07bf4ee27bb6f5d139eb575f2b7&oe=5E5E2DDA" />
             <p>Jake is 4th year ECE major with tech cores in Software and Integrated Circuits. Jake is
             responisble for front-end design. </p>
-            <p>Jake has a total of 20 commits and 1 unit test</p>
+            <p>Jake has a total of 31 commits and 1 unit test</p>
           </div>
 
           <br></br>
@@ -44,7 +44,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-1/1907918_791566744241977_2062132824891958511_n.jpg?_nc_cat=106&_nc_oc=AQn88i1WNq1I-8TOxcXhBMmcviEaFbAlOmbCUHWb84VWtOODCEw3Jng2fgV8bSESKrQ&_nc_ht=scontent.fftw1-1.fna&oh=02a6ed019575dc3246adea64c1a1b008&oe=5E271829" />
             <p>Sakar Khadka is a 4th year ECE major with tech cores in Software and Academic Enrichment.
             He is repsonsible for back-end design. </p>
-            <p>Sakar has a total of 1 commits and 0 unit tests.</p>
+            <p>Sakar has a total of 2 commits and 0 unit tests.</p>
           </div>
 
           <br></br>
@@ -55,7 +55,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-9/46002440_1980562665342373_3934667561894936576_n.jpg?_nc_cat=101&_nc_oc=AQn3pzfnCKcShZhYU7WGj2pquFgI8mMzL4yqqShK1gGJZQHr3YOE9h92vF5LMlYs-Z0&_nc_ht=scontent.fftw1-1.fna&oh=0f58ec3ca9534cfbcbc1817d89f6b547&oe=5E52A814" />
             <p>Shishir Khadka is a 4th year ECE major with tech cores in Software and Academic Enrichment.
             He is responsible for back-end design. </p>
-            <p>Shishir has a total of 0 commits and 0 unit tests.</p>
+            <p>Shishir has a total of 2 commits and 0 unit tests.</p>
           </div>
 
           <br></br>
@@ -66,7 +66,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-9/14034775_1745285052411444_1249948486777613532_n.jpg?_nc_cat=110&_nc_oc=AQkFDpluYXFXgC4vmHfZGbcExpzu0IPiNWBuRJ92pu6rk6eC6awHzZ0FZTx8cjXe5Q4&_nc_ht=scontent.fftw1-1.fna&oh=f8b9d8bd1b770ef46362e994f1b475e1&oe=5E64EA34" />
             <p>Esha Makwana is a 4th year ECE major with tech cores in Software and Academic Enrichment.
             She is responsible for front-end design.</p>
-            <p>Esha has a total of 0 commits and 0 unit tests</p>
+            <p>Esha has a total of 2 commits and 0 unit tests</p>
           </div>
 
           <br></br>
@@ -88,7 +88,7 @@ export default class About extends Component {
             <img src="https://scontent.fftw1-1.fna.fbcdn.net/v/t1.0-9/22851798_1661196610597772_5777857504645349981_n.jpg?_nc_cat=100&_nc_oc=AQnLJFSI99xnG6Q8Pi_558s0DEscvRs0WE4o_gajnrtp5oweZjXT6s1F6Q294Bk_ZRY&_nc_ht=scontent.fftw1-1.fna&oh=66924c00261573630ef86f0c78b4b51d&oe=5E1895E2" />
             <p>Maximiliano Perez is a 4th year ECE major with tech cores in Power Systems and Software.
             He is responsible for the back-end design. </p>
-            <p>Maximiliano has a total of 8 commits and 0 unit tests.</p>
+            <p>Maximiliano has a total of 11 commits and 6 unit tests.</p>
           </div>
         </div>
 
@@ -122,9 +122,9 @@ export default class About extends Component {
         <div>
           <h2>Stats</h2>
           <ul>
-            <li>Total Commits:39</li>
-            <li>Total Unit Tests: 1</li>
-            <li>Total Issues: 10 </li>
+            <li>Total Commits:95</li>
+            <li>Total Unit Tests: 2</li>
+            <li>Total Issues: 15 </li>
           </ul>
         </div>
       </React.Fragment>
